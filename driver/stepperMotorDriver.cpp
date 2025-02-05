3b05b1ef6ada1a8e5679efb9bbedf557804253fb
Mingjie.Zhu <zhumingjie0627@outlook.com>
2025-02-05 17:07:39 +0000

    driver -v1

--- driver/stepperMotorDriver.cpp
+++ driver/stepperMotorDriver.cpp
@@ -1,36 +1,52 @@
-class MotorDriver {
-    public:
-    MotorDriver(){};
-    ~MotorDriver(){};
+#include "stepperMotorDriver.h"
 
-    void configure(short en, short step, short dir) {
-    };
+void MotorDriver::configure(short en, short step, short dir) {
+    EN_PIN = en;
+    STEP_PIN = step;
+    DIR_PIN = dir;
+    pinMode(EN_PIN, INPUT);
+    pinMode(STEP_PIN, OUTPUT);
+    pinMode(DIR_PIN, OUTPUT);
+};
 
-    void forward(unsigned long pulse, unsigned int feedrate) {
-    };
+void MotorDriver::forward(unsigned long pulse, unsigned int feedrate) {
+    digitalWrite(DIR_PIN, HIGH^REVERT_CALIBRATE);
+    unsigned int delay = (unsigned int) 60000000 / (feedrate * STEP_PER_MM);
+    sendPulse(pulse, delay);
+    ABS_POS_PULSE += pulse;   
+};
 
-    void backward(unsigned long pulse, unsigned int feedrate) {
-    };
+void MotorDriver::backward(unsigned long pulse, unsigned int feedrate) {
+    digitalWrite(DIR_PIN, LOW^REVERT_CALIBRATE);
+    unsigned int delay = (unsigned int) 60000000 / (feedrate * STEP_PER_MM);
+    sendPulse(pulse, delay);
+    ABS_POS_PULSE -= pulse;
+};
 
-    void sendPulse(unsigned long pulse, unsigned int delay) {
-    };
-    
-    void toOrigin(){
-    };
+void MotorDriver::sendPulse(unsigned long pulse, unsigned int delay) {
+    for(unsigned long i = 0; i < pulse; i++) {
+        digitalWrite(STEP_PIN, HIGH);
+        delayMicroseconds(delay);
+        digitalWrite(STEP_PIN, LOW);
+        delayMicroseconds(delay);
+    }
+};
 
-    void toPosition(float pos, unsigned int feedrate) {
-    };
+void MotorDriver::toOrigin(){
+};
 
-    void setCoordinate(float pos) {
-    };
-    
+void MotorDriver::toPosition(float pos, unsigned int feedrate) {
+    unsigned long targetPulse = pos * STEP_PER_MM;
+    long dPulse = (long) targetPulse - ABS_POS_PULSE + OFFSET_POS_PULSE;
+    if (dPulse > 0) forward(dPulse, feedrate);
+    else if (dPulse < 0) backward(dPulse, feedrate);        // TODO
+};
 
-    private:
-    short EN_PIN;
-    short STEP_PIN;
-    short DIR_PIN;
-    unsigned long ABS_POS_PULSE;
-    unsigned long OFFSET_POS_PULSE;
-    unsigned int STEP_PER_MM = 6;
-}
+void MotorDriver::setCoordinate(float pos) {
+    OFFSET_POS_PULSE = ABS_POS_PULSE - pos * STEP_PER_MM;
+};
+
+void MotorDriver::invertDirection(){
+    REVERT_CALIBRATE = !REVERT_CALIBRATE;
+};
 
--- driver/stepperMotorDriver.h
+++ driver/stepperMotorDriver.h
@@ -1,31 +1,27 @@
 #ifndef MOTOR_DRIVER_H
 #define MOTOR_DRIVER_H
+#include <Arduino.h>
 
 class MotorDriver {
     public:
     MotorDriver(){};
     ~MotorDriver(){};
 
-    void configure(short en, short step, short dir) {
-    };
+    void configure(short en, short step, short dir);
 
-    void forward(unsigned long pulse, unsigned int feedrate) {
-    };
+    void forward(unsigned long pulse, unsigned int feedrate) ;
 
-    void backward(unsigned long pulse, unsigned int feedrate) {
-    };
+    void backward(unsigned long pulse, unsigned int feedrate) ;
 
-    void sendPulse(unsigned long pulse, unsigned int delay) {
-    };
+    void sendPulse(unsigned long pulse, unsigned int delay) ;
     
-    void toOrigin(){
-    };
+    void toOrigin();
 
-    void toPosition(float pos, unsigned int feedrate) {
-    };
+    void toPosition(float pos, unsigned int feedrate) ;
 
-    void setCoordinate(float pos) {
-    };
+    void setCoordinate(float pos) ;
+
+    void invertDirection();
     
 
     private:
@@ -35,9 +31,7 @@ class MotorDriver {
     unsigned long ABS_POS_PULSE;
     unsigned long OFFSET_POS_PULSE;
     unsigned int STEP_PER_MM = 6;
-}
-
-
-
+    bool REVERT_CALIBRATE = 0;
+};
 
 #endif  
